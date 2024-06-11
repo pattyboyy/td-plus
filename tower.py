75d50417cf97d16faaabf96c18c6f312ae946a3c
pattyboyy <134738449+pattyboyy@users.noreply.github.com>
2024-06-11 13:48:38 -0600

    Updated Tower class to include shooting projectiles at enemies within range.

--- tower.py
+++ tower.py
@@ -1,14 +1,32 @@
 import pygame
+from projectile import Projectile
 
 class Tower:
-    def __init__(self, position):
+    def __init__(self, position, game):
         self.position = position
-        self.range = 100
+        self.range = 150
+        self.game = game
+        self.projectiles = []
         self.damage = 10
 
     def update(self):
         # Logic to find and shoot at enemies
-        pass
+        for enemy in self.game.enemies:
+            if self.in_range(enemy):
+                self.shoot(enemy)
+
+        for projectile in self.projectiles:
+            projectile.update()
 
     def draw(self, screen):
         pygame.draw.circle(screen, (0, 255, 0), self.position, 20)
+    def in_range(self, enemy):
+        distance = ((self.position[0] - enemy.position[0]) ** 2 + (self.position[1] - enemy.position[1]) ** 2) ** 0.5
+        return distance <= self.range
+
+    def shoot(self, enemy):
+        projectile = Projectile(self.position, enemy)
+        self.projectiles.append(projectile)
+        pygame.draw.circle(screen, (0, 255, 0), self.position, 20)
+        for projectile in self.projectiles:
+            projectile.draw(screen)
